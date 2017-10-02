e7ce327569b88da3f519689b766849984696a16f
Nick Ruiz <nickruiz210@gmail.com>
2017-10-01 22:42:44 -0500

    Updated Card.java and removed stuff we didn't need

--- src/warGame/Card.java
+++ src/warGame/Card.java
@@ -1,30 +1,14 @@
 package warGame;
 
 public class Card {
-
-	/* Codes for the 4 suits.*/
-	public final static int SPADES = 0, 
-							HEARTS = 1, 
-							DIAMONDS = 2, 
-							CLUBS = 3;
-
-	/* constant values for non numeric card values */
-	public final static int ACE = 1, 
-							JACK = 11, 
-							QUEEN = 12, 
-							KING = 13;
 	
-	/* suit of the card, which will use one of the constants
-	 * 0 = SPADES, 1 = HEARTS, 2 = DIAMONDS, or 3 = CLUBS */
+	private final int cardValue; 
 	private final int cardSuit; 
 							
-	/* value of the card */
-	private final int cardValue; 
-
 	/**
 	 * Constructor for card with a given value and suit
-	 * @param cardValue an integer between 1 and 13
-	 * @param cardSuit an integer between 0 and 3
+	 * @param cardValue, an integer between 1 and 13
+	 * @param cardSuit, an integer between 0 and 3
 	 */
 	public Card(int cardValue, int cardSuit) {
 		this.cardValue = cardValue;
@@ -61,31 +45,31 @@ public class Card {
 			case 13:
 				return "KING";
 			default:
-				return "INVALID VALUE";
+				return "INVALID CARD VALUE";
 		}
 	}
 
 	public String convertCardSuitToString() {
 		
 		switch (cardSuit) {
-			case SPADES:
+			case 0:
 				return "SPADES";
-			case HEARTS:
+			case 1:
 				return "HEARTS";
-			case DIAMONDS:
+			case 2:
 				return "DIAMONDS";
-			case CLUBS:
+			case 3:
 				return "CLUBS";
 			default:
-				return "INVALID SUIT";
+				return "INVALID CARD SUIT";
 		}
 	}
 
 	/**
-	 * Returns a String representation of this card.
+	 * Displays a String representation of this card
 	 * in the format "TEN of HEARTS, JACK of DIAMONDS"
 	 */
-	public String toString() {
+	public String displayCard() {
 		return convertCardValueToString() + " of " + convertCardSuitToString();
 	}
 }
