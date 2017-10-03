f4e3820b79cf840c26526058affb274a8b93fce7
Rbravo3443 <richard-807@hotmail.com>
2017-10-03 16:31:01 -0500

    Added some stuff

--- src/warGame/Hand.java
+++ src/warGame/Hand.java
@@ -1,5 +1,30 @@
 package warGame;
 
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author richard
+ *
+ */
 public class Hand {
 
+	private ArrayList<Card> CurrentHand;
+	
+	public Hand(){
+	     CurrentHand = new ArrayList<Card>();
+	}
+	
+	public Card PlayCard(Card Card){
+		return Card;
+	}
+	
+	public void DrawCard(Card card){
+		CurrentHand.add(card);
+	}
+	
+	
+	
+	
+	
 }
--- src/warGame/Player.java
+++ src/warGame/Player.java
@@ -1,5 +1,27 @@
 package warGame;
-
+/**
+ * 
+ * @author richard
+ *
+ */
 public class Player {
 
+	private Hand HandOfPlayer;
+	
+	private int TotalPoints;
+	
+	public Player(){
+		TotalPoints = 0;
+		HandOfPlayer = null;
+	}
+	
+	public void InvokePlay(){
+		
+	}
+	
+	public void InvokeDraw(){
+		
+	}
+	
+	
 }
