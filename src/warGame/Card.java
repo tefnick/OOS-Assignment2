dee0885199b370b3522da3cd83e4af45a5198dba
Nick Ruiz <nickruiz210@gmail.com>
2017-10-02 20:59:40 -0500

    Rewrote Card.java using enums as suggested by Professor

--- src/warGame/Card.java
+++ src/warGame/Card.java
@@ -1,117 +1,42 @@
 package warGame;
 
+import java.util.*;
+import java.awt.List;
+
 public class Card {
 	
-	private final int cardValue; 
-	private final int cardSuit; 
-							
-	/**
-	 * Constructor for card with a given value and suit
-	 * @param cardValue, an integer between 1 and 13
-	 * @param cardSuit, an integer between 0 and 3
-	 */
-	public Card(int cardValue, int cardSuit) {
-		this.cardValue = cardValue;	//Question: Should we randomize a card value when it gets initialized?
-		this.cardSuit = cardSuit;   //			instead of hard coding it?
-	}
+    public enum Rank { TWO, THREE, FOUR, FIVE, SIX,
+        SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE }
 
-	public String convertCardValueToString() {
-		
-		switch (cardValue) {
-			case 1:
-				return "ACE";
-			case 2:
-				return "TWO";
-			case 3:
-				return "THREE";
-			case 4:
-				return "FOUR";
-			case 5:
-				return "FIVE";
-			case 6:
-				return "SIX";
-			case 7:
-				return "SEVEN";
-			case 8:
-				return "EIGHT";
-			case 9:
-				return "NINE";
-			case 10:
-				return "TEN";
-			case 11:
-				return "JACK";
-			case 12:
-				return "QUEEN";
-			case 13:
-				return "KING";
-			default:
-				return "INVALID CARD VALUE";
-		}
-	}
+    public enum Suit { CLUBS, DIAMONDS, HEARTS, SPADES }
 
-	public String convertCardSuitToString() {
-		
-		switch (cardSuit) {
-			case 0:
-				return "SPADES";
-			case 1:
-				return "HEARTS";
-			case 2:
-				return "DIAMONDS";
-			case 3:
-				return "CLUBS";
-			default:
-				return "INVALID CARD SUIT";
-		}
-	}
-	
-	public int getCardSuit(){
-		return this.cardSuit;
-	}
-	
-	public int getCardValue(){
-		return this.cardValue;
-	}
+    private final Rank rank;
+    private final Suit suit;
+    
+    /**
+     * Constructor
+     * @param rank, Rank type of this card
+     * @param suit, Suit type of this card
+     */
+    private Card(Rank rank, Suit suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
 
-	/**
-	 * Displays a String representation of this card
-	 * in the format "TEN of HEARTS, JACK of DIAMONDS"
-	 */
-	public String displayCard() {
-		return convertCardValueToString() + " of " + convertCardSuitToString();
-	}
+    public Rank getRank() { 
+    	return rank; 
+    }
+    
+    public Suit getSuit() { 
+    	return suit; 
+    }
+    
+    /**
+     * Converts the card Rank and Suit to a String literal
+     * Example: "JACK of DIAMONDS" or "TEN of HEARTS"
+     * @return String representation of this card
+     */
+    public String toString() { 
+    	return rank + " of " + suit; 
+    }
 }
-
-
-//import java.util.*;
-//
-//public class Card {
-//    public enum Rank { DEUCE, THREE, FOUR, FIVE, SIX,
-//        SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE }
-//
-//    public enum Suit { CLUBS, DIAMONDS, HEARTS, SPADES }
-//
-//    private final Rank rank;
-//    private final Suit suit;
-//    private Card(Rank rank, Suit suit) {
-//        this.rank = rank;
-//        this.suit = suit;
-//    }
-//
-//    public Rank rank() { return rank; }
-//    public Suit suit() { return suit; }
-//    public String toString() { return rank + " of " + suit; }
-//
-//    private static final List<Card> protoDeck = new ArrayList<Card>();
-//
-//    // Initialize prototype deck
-//    static {
-//        for (Suit suit : Suit.values())
-//            for (Rank rank : Rank.values())
-//                protoDeck.add(new Card(rank, suit));
-//    }
-//
-//    public static ArrayList<Card> newDeck() {
-//        return new ArrayList<Card>(protoDeck); // Return copy of prototype deck
-//    }
-//}
