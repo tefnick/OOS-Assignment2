550316d11a9493b4c7bf19d754b6d975afc308ca
Moyoosore Akinrinmade <afolasadeomoakin@gmail.com>
2017-10-06 10:58:18 -0500

    Added values to the ranks in cars.java

--- src/warGame/Card.java
+++ src/warGame/Card.java
@@ -31,6 +31,52 @@ public class Card {
     public Suit getSuit() { 
     	return suit; 
     }
+    public int assignRankValue(){
+    	int value;
+    	switch(rank){
+    	case TWO:
+    		value = 2;
+    		break;
+    	case THREE:
+    		value = 3;
+    		break;
+    	case FOUR:
+    		value = 4;
+    		break;
+    	case FIVE:
+    		value = 5;
+    		break;
+    	case SIX:
+    		value = 6;
+    		break;
+    	case SEVEN:
+    		value = 7;
+    		break;
+    	case EIGHT:
+    		value = 8;
+    		break;
+    	case NINE:
+    		value = 9;
+    		break;
+    	case TEN:
+    		value = 10;
+    		break;
+    	case JACK:
+    		value=11;
+    		break;
+    	case QUEEN:
+    		value = 12;
+    		break;
+    	case KING:
+    		value= 13;
+    		break;
+    	case ACE:
+    		value =14;
+    		break;
+    	}
+		return 0;
+    	
+    }
     
     /**
      * Converts the card to a String literal
